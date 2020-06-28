65ecb39080912f75d9bde7f18ca5c7913968646a
campbelllssoup <matjocampbell@gmail.com>
2020-06-28 16:32:06 -0700

    Completes circular linked list detection function.

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,27 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+// it's important to be able to detect circular lists, because all of our list
+// methods assume that we have a singly linked, non-circular list.
+
+
+function circular(list) {
+  if (list.head == null) { return false; }
+  // initialization
+  let slow = list.head;
+  let fast = list.head;
+
+  while (fast.next && fast.next.next) {
+    // iteration
+    slow = slow.next;
+    fast = fast.next.next;
+    // check for circularity
+    if (slow === fast) { return true; }
+  }
+
+  // some node's #next call equals null
+  return false;
+}
 
 module.exports = circular;
+
--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -13,21 +13,24 @@
 //   midpoint(l); // returns { data: 'b' }
 
 
-// take in a list, return the middle node in the list
-// use the n + 1, m + 2 trick until m == null
-// since we can't use numbers, you're keeping a reference to every node +
-// every second node. When your m ref for every second node is going to be equal,
-// you can return the n ref as the midpoint.
+/* 
 
-// draw it out if this doesn't make sense to you or others
+  take in a list, return the middle node in the list
+  use the n + 1, m + 2 trick until m == null
+  since we can't use numbers, you're keeping a reference to every node +
+  every second node. When your m ref for every second node is going to be equal,
+  you can return the n ref as the midpoint.
 
-// if list is even, use the length / 2 - 1 index node.
+  draw it out if this doesn't make sense to you or others
+
+  if list is even, use the length / 2 - 1 index node. 
+
+*/
 
 function midpoint(list) {
   // edge cases, list is empty. list contains only one node
-  if (!list.head || !list.head.next) { return this.head; }
   let countByOneNode = list.head;
-  let countByTwoNode = list.head.next;
+  let countByTwoNode = list.head;
 
   // iteration
   while (countByTwoNode.next && countByTwoNode.next.next) {
@@ -35,13 +38,7 @@ function midpoint(list) {
     countByTwoNode = countByTwoNode.next.next;
   }
 
-  // resolution
-  let isOddLength = !!countByTwoNode.next;
-  if (isOddLength) {
-    return countByOneNode.next;
-  } else {
-    return countByOneNode;
-  }
+  return countByOneNode;
 }
 
 module.exports = midpoint;
