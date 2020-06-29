87c60e07f186e0a1692b31c4c01b1b94a960080f
campbelllssoup <matjocampbell@gmail.com>
2020-06-28 23:55:34 -0700

    Solves BST exercise.

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,52 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(newData) {
+    // compare input data with current Node data until in position
+    // I went with just inserting at the ends of the binary tree.... what about
+    // inserting node into the middle of one? would that likely be more efficient?
+
+    let currNode = this;
+    while (currNode) {
+      if (newData < currNode.data) {
+        if (!currNode.left) {
+          currNode.left = new Node(newData); 
+          currNode = null;
+        } else { 
+          currNode = currNode.left; 
+        }
+      } else {
+        if (!currNode.right) {
+          currNode.right = new Node(newData);
+          currNode = null;
+        } else { 
+          currNode = currNode.right;
+        }
+      }
+    }
+  }
+  
+  contains(seekData) {
+    let currNode = this;
+    while (currNode) {
+      if (seekData === currNode.data) {
+        return currNode;
+      } else if (seekData < currNode.data) {
+          currNode = currNode.left; 
+      } else {
+          currNode = currNode.right;
+      }
+    }
+    return null;
+  }
+
+}
 
 module.exports = Node;
