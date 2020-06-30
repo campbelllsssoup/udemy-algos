f87878ddddf95472d0fbbdf42eb18c7a80b67f62
campbelllssoup <matjocampbell@gmail.com>
2020-06-29 19:03:25 -0700

    Completes events and BST validation exercises

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,33 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [callback]
+    } else {
+      this.events[eventName].push(callback);
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (const el of this.events[eventName]) {
+        el();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.events[eventName] = [];
+  }
 }
 
 module.exports = Events;
--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,18 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // check if current node's data satisfies BST rule.
+  if (min && node.data < min) { return false; }
+  if (max && node.data > max) { return false; }
+  
+  // recurse through the subtrees, passing along the current data's info as
+  // a new set of bounds.
+  if (node.right && !validate(node.right, node.data, max)) { return false; }
+  if (node.left && !validate(node.left, min, node.data)) { return false; }
+  
+  // if no return statements are hit, each node satisfies the rule.
+  return true;
+}
 
 module.exports = validate;
